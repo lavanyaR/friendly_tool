eee44b58f0a5f7cb9804db189a6d1a2e75a2c041
lavanya <lavanya@indigenius.com>
2013-02-01 13:40:56 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendlyTool::Application.config.secret_token = '9cde9130b3f7f84d905359cd0318c95e586a0494ffb062e9397076b45168582e03ffd94a310184d4ddf14287c90bafbf251a63edbe1529d9bc238a5fe8e4bcd3'

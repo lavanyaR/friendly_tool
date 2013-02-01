3d66cf0740980bf951dfd0a916a987b7e8550229
lavanya <lavanya@indigenius.com>
2013-02-01 15:29:29 +0530

    chnages in tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,16 @@ class Task < ActiveRecord::Base
   belongs_to :admin_user
   validates :title, :project_id, :admin_user_id, :presence => true
   validates :is_done, :inclusion => { :in => [true, false] }
+
+
+  scope :all, :default => true
+  scope :due_this_week do |tasks|
+    tasks.where('due_date > ? and due_date < ?', Time.now, 1.week.from_now)
+  end
+  scope :late do |tasks|
+    tasks.where('due_date < ?', Time.now)
+  end
+  scope :mine do |tasks|
+    tasks.where(:admin_user_id => current_admin_user.id)
+  end
 end
